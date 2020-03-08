8a77cff94ebc00c8d05c95ab2f3e6bec11c819ce
taohsu <ixmlmxi@gmail.com>
2020-03-08 23:02:49 +0100

    +

--- sketch.js
+++ sketch.js
@@ -1,7 +1,11 @@
-var city = 'Kortrijk';
+var url = window.location.search;
+var city = getQueryVariable("city");
+var date = getQueryVariable("date");
+var antenna = getQueryVariable("antenna");
+//var city = 'Kortrijk';
 // var city = 'Brussels';
-var date = '2019-10-19';
-var antenna = '4g';
+//var date = '2019-10-19';
+//var antenna = '4g';
 var sitedata;
 var consodata;
 // var sitedata = 'Kortrijk_2019-10-01_site_4g.json';
@@ -216,4 +220,15 @@ image(img, 0, 0);
   text('Antenna energy consumption hourly: ' + energyPerhour + ' KWh / h', 20, 100);
   text('Antenna energy consumption total: ' + energyTotal + ' KWh', 20, 120);
 
+}
+
+function getQueryVariable(variable)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
 }
