2b75e09aa1a19233cba2a2ada2df4a78da84c98a
taohsu <ixmlmxi@gmail.com>
2020-04-09 22:03:32 +0200

    +

--- sketch.js
+++ sketch.js
@@ -215,7 +215,7 @@ image(img, 0, 0);
       for (var x = 1; x < N + 1; x++) {
         var d = sqrt(sq((x - 1) * w - locationx[i]) + sq((y - 1) * h - locationy[i]));
 //        var s = 0.3 * pow(sq(max(width - d*4*reso, 0) / width), map(trafficall[i][index], tminMax[1], tminMax[0], 2, 20));
-         var s = 0.5 * pow(max(width - d*4*reso, 0) / width, map(trafficall[i][index], tminMax[1], tminMax[0], 10, 20));
+         var s = 0.5 * pow(max(width - d*4*reso, 0) / width, map(trafficall[i][index], tminMax[1], tminMax[0], 1, 15));
         // var c = color(frameCount % 360, 100, 100);
         // var hue = round(map(energyall[i][index], 0.5, 1.6, 180, 360)) % 360;
         // console.log(hue);
@@ -267,7 +267,7 @@ image(img, 0, 0);
   textSize(10);
   colorMode(RGB, 255, 255, 255);
   fill(255, 255, 255);
-  text('10:73', 10, height - 200);
+  text('10:74', 10, height - 200);
   text(city + ' ' + date + ' ' + daydayday + ' ' + index + 'h', 10, height - 175);
   text(antenna + '_sites: ' + siteCount, 10, height - 160);
 
