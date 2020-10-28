1899f3a8bb471b0ef9f70386170215e4e7d70336
taohsu <ixmlmxi@gmail.com>
2020-10-28 15:40:57 +0100

    +

--- sketch.js
+++ sketch.js
@@ -312,7 +312,7 @@ image(img, 0, 0);
 
   fill(255);
   push();
-  translate(160,556);
+  translate(150,556);
   textSize(9);
   text('DATA TRAFFIC ', 0, -10);
   for (var i = 0; i < index; i++) {
@@ -328,7 +328,7 @@ image(img, 0, 0);
   pop();
   
   push();
-  translate(330,556);
+  translate(340,556);
   textSize(9);
   text('ENERGY COMSUMPTION ', 0, -10);
   for (var i = 0; i < index; i++) {
